360251b5fb74c49b0478717b635c99d3749f1eed
Thiago <t.r.goveia@gmail.com>
2025-02-11 09:43:40 -0300

    chore reorganize files

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { DocumentProcessModule } from './../src/document-process.module';
+import { App } from 'supertest/types';
+import { AppModule } from '../src/app.module';
 
-describe('DocumentProcessController (e2e)', () => {
-  let app: INestApplication;
+describe('AppController (e2e)', () => {
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [DocumentProcessModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -16,9 +17,6 @@ describe('DocumentProcessController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
--- apps/workflow-process/test/app.e2e-spec.ts
+++ apps/workflow-process/test/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { DocumentProcessModule } from '../src/document-process.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('DocumentProcessController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [DocumentProcessModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
