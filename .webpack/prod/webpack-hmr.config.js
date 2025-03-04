df9903d4d38a47616e0e338cea1c8a1a1b055361
Thiago <t.r.goveia@gmail.com>
2025-03-04 16:15:11 -0300

    fix: webpack configs for production

--- .webpack/prod/webpack-hmr.config.js
+++ .webpack/prod/webpack-hmr.config.js
@@ -1,20 +1,16 @@
+const nodeExternals = require('webpack-node-externals');
 const webpack = require('webpack');
-const path = require('path');
 
 module.exports = function (options) {
   const { plugins, ...config } = options;
   return {
     ...config,
-    output: {
-      ...config.output,
-      path: path.resolve(__dirname, 'dist/apps/process-document-job'),
-      filename: 'lambda.js',
-      // libraryTarget: 'commonjs2',
-      library: {
-         type: 'commonjs2'
-      },
-    },
-    externals: [],
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
     plugins: [
       ...plugins,
       new webpack.IgnorePlugin({
@@ -22,8 +18,6 @@ module.exports = function (options) {
           const lazyImports = [
             '@nestjs/microservices',
             'cache-manager',
-            // 'class-validator',
-            // 'class-transformer',
             '@nestjs/websockets/socket-module',
             '@nestjs/microservices/microservices-module',
             'fastify-swagger',
