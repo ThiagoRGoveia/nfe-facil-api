80774e5b229ad60c0c218cf2047d22048e22fb08
Thiago <t.r.goveia@gmail.com>
2025-05-23 16:06:00 -0300

    fix: jwt strategy to use proxy

--- apps/api/src/infra/auth/jwt.strategy.ts
+++ apps/api/src/infra/auth/jwt.strategy.ts
@@ -3,15 +3,19 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { passportJwtSecret } from 'jwks-rsa';
 import { ConfigService } from '@nestjs/config';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private readonly configService: ConfigService) {
+    const nodeEnv = configService.get('NODE_ENV');
     const AUTH_DOMAIN = configService.get('AUTH_DOMAIN');
     const AUTH_ISSUER_URL = configService.get('AUTH_ISSUER_URL');
     const AUTH_AUDIENCE = configService.get('AUTH_AUDIENCE');
+    const PROXY_URL = configService.get('PROXY_URL');
+    const PROXY_PORT = configService.get('PROXY_PORT');
 
-    if (!AUTH_DOMAIN || !AUTH_ISSUER_URL || !AUTH_AUDIENCE) {
+    if (!AUTH_DOMAIN || !AUTH_ISSUER_URL || !AUTH_AUDIENCE || !PROXY_URL || !PROXY_PORT) {
       throw new Error('AUTH_DOMAIN or AUTH_ISSUER_URL or AUTH_AUDIENCE is not set');
     }
 
@@ -21,6 +25,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
         rateLimit: true,
         jwksRequestsPerMinute: 5,
         jwksUri: `${AUTH_DOMAIN}/.well-known/jwks.json`,
+        requestAgent: nodeEnv !== 'local' ? new HttpsProxyAgent(`http://${PROXY_URL}:${PROXY_PORT}`) : undefined,
       }),
 
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
